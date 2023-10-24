e1583a4ccbf604118ee434f35d8a691265b3ba8d
­박찬영 / 학생 / 자유전공학부 <pcy1203@snu.ac.kr>
2023-10-24 14:29:15 +0000

    [FEAT] Polaroid Example, Polaroid Color Selection

--- controllers/polaroid.js
+++ controllers/polaroid.js
@@ -12,7 +12,6 @@ exports.renderPolaroids = async (req, res, next) => {
   res.render('polaroid/view', { polaroids });
 };
 
-
 exports.renderPolaroid = async (req, res, next) => {
   const id = req.params.id;
   const polaroid = await Polaroid.findOne({
@@ -35,11 +34,13 @@ exports.renderSuccess = async (req, res, next) => {
 
 exports.writePolaroid = async (req, res, next) => {
   // TODO - 유효성 검사 추가하기
-  const { content } = req.body;
+  const { content, color, size } = req.body;
   try {
 	const polaroid = await Polaroid.create({
 	  image: `polaroid/${req.file.filename}`,
 	  content,
+	  color,
+	  size,
 	  writer: req.user.dataValues.id,
 	});
     return res.redirect(`/myself/polaroid/${polaroid.id}/success`);
