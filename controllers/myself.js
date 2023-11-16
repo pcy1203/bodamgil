1ce604ca2ebb60e5eb327d1503fde01688c40a80
­박찬영 / 학생 / 자유전공학부 <pcy1203@snu.ac.kr>
2023-11-16 13:19:12 +0000

    [FEAT] Faq & Notice Content Open, Myself Game List

--- controllers/myself.js
+++ controllers/myself.js
@@ -1,7 +1,9 @@
+const { gameList } = require('../models/initialdata');
+
 exports.renderMain = (req, res, next) => {
   res.render('myself/main');
 };
 
 exports.renderGame = (req, res, next) => {
-  res.render('myself/game');
+  res.render('myself/game', { gameList });
 }
